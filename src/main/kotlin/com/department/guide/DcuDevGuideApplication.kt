9268b92e206b04c4337c0fca762fef0008e6305d
cprtmchzh <bin54511563@gmail.com>
2024-07-25 11:29:26 +0900

    첫 커밋

---
+++ src/main/kotlin/com/department/guide/DcuDevGuideApplication.kt
@@ -0,0 +1,11 @@
+package com.department.guide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DcuDevGuideApplication
+
+fun main(args: Array<String>) {
+	runApplication<DcuDevGuideApplication>(*args)
+}
